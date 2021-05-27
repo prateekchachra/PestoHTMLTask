1a2bfeaecb8687baaa86890e3d1393cedb923006
Prateek Chachra <prateekchachra@live.com>
2021-05-27 18:59:14 +0530

    Initiated react project and finished the HTML one

--- HTML_App/script.js
+++ HTML_App/script.js
@@ -1,6 +1,4 @@
 
-
-
 //Form and Navigator references
 
 const contactForm = document.getElementById('contactForm');
@@ -8,15 +6,122 @@ const navRef = document.getElementById('navigator');
 
 //Getting the data
 
+$.getJSON('data/albums.json', (data) => {
+		let tableContainer = $("#table-container");
 
-$.getJSON('data/albums.json', (data) => console.log(data)).fail((jqXHR, textStatus, error) => console.log(textStatus))
+		let tableElement = $("<table>").attr("id", "albums-table");
 
-// const wavesAboutSongsData = require('data/wavesaboutsongs.json');
-// const countries = require('data/countries.json');
-// const wavesAlbumData = require('data/wavesofparamnesia.json');
-// const regretsAlbumData = require('data/regretsandgrievances.json');
+		tableElement.append($("<thead>").append($("<tr>").append($("<th>").html("Name")).append($("<th>").html("Type")).append($("<th>").html("Release Year")).append($("<th>").html("Number Of songs")).append($("<th>").html("Poster")).append($("<th>").html("Spotify Link"))))
+
+		let tableBody = $("<tbody>");
+
+		for(var i = 0; i < data.length; i++){
+
+			let albumTitle = data[i].title;
+			let titleCell = $("<td>").html(albumTitle);
+			let typeCell = $("<td>").html(data[i].type);
+			let yearCell = $("<td>").html(data[i].year);
+			let numberCell = $("<td>").html(data[i].numberOfSongs);
+
+			let posterImageElement = $("<img>").attr("src", data[i].image).attr("alt", albumTitle + " Album Art").addClass("album-art-img");
+			let posterFigureElement = $("<figure>").append(posterImageElement).append($("<figcaption>").html(albumTitle + " - Album Art"));
+			let posterCell = $("<td>").append(posterFigureElement);
+
+			let spotifyLinkElement = $("<a>").attr("href", data[i].link).attr("aria-label", albumTitle + " Spotify").attr("target", "_blank").html(albumTitle);
+			let spotifyLinkCell = $("<td>").append(spotifyLinkElement);
+
+			let tableRow = $("<tr>").append(titleCell).append(typeCell).append(yearCell).append(numberCell).append(posterCell).append(spotifyLinkCell);
+
+			tableBody.append(tableRow);
+		}
+		tableElement.append(tableBody)
+
+		tableContainer.html("");
+		tableContainer.append(tableElement);
+}).fail((jqXHR, textStatus, error) => console.log(textStatus))
 
 
+$.getJSON('data/countries.json', (data) => {
+	let countriesSelect = document.getElementById('country');
+	data.map((item, index) => {
+		countriesSelect.innerHTML += '<option value="' + item.value + '">' + item.label + '</option>';
+	})
+
+
+
+}).fail((jqXHR, textStatus, error) => console.log(error))
+
+
+
+
+$.getJSON('data/wavesaboutsongs.json', (data) => {
+
+	let headerElement = $("<h2>").html('About the songs');
+	let mainAboutWaves = $("<main>").append(headerElement);
+
+	for(var i = 0; i < data.length; i++){
+	
+		let youtubeIframeDiv = $("<div>").addClass("iframe-container").append($("<iframe>").attr("src", data[i].youtubeLink).attr("title", data[i].title).attr("frameborder", "0").attr("allow", "clipboard-write; encrypted-media; gyroscope; picture-in-picture").attr("allowfullscreen", null).addClass("youtube-iframe"));
+		let articleElement = $("<article>").append($("<h4>").append(data[i].title)).append($("<p>").attr("role", "text").html(data[i].description)).append(youtubeIframeDiv);
+
+		mainAboutWaves.append(articleElement)
+	}
+
+	let wavesAboutSection = $("#about-waves");
+	wavesAboutSection.html = "";
+
+	wavesAboutSection.append(mainAboutWaves);
+
+}).fail((jqXHR, textStatus, error) => console.log(textStatus))
+
+
+$.getJSON('data/wavesofparamnesia.json', (data) => {
+	let mainWaves = renderAlbumDetails(data);
+	let wavesSection = $("#waves");
+	wavesSection.html = "";
+
+	wavesSection.append(mainWaves)
+
+}).fail((jqXHR, textStatus, error) => console.log(textStatus))
+
+
+$.getJSON('data/regretsandgrievances.json', (data) => {
+	let mainRegrets = renderAlbumDetails(data);
+	let regretsSection = $("#regrets");
+	regretsSection.html = "";
+
+	regretsSection.append(mainRegrets)
+
+}).fail((jqXHR, textStatus, error) => console.log('REGRETS', 'REGRETS'))
+
+
+function renderAlbumDetails(data){
+	let headerElement = $("<h2>").html(data.title);
+	let artworkElement = data.artwork ? $("<img>").attr("src", data.artwork).addClass("artwork-img") : "";
+
+	let detailsListHeader = $("<p>").addClass("list-header").html("What is in this album?")
+	let detailsList = $("<ul>");
+
+	for(var i = 0; i < data.inThisAlbum.length; i++){
+		detailsList.append($("<li>").append($("<span>").html(data.inThisAlbum[i])))
+	}
+
+	let mustListenHeader = $("<p>").addClass("list-header").html("Must Listen Tracks from this Album")
+	let mustListenList = $("<ul>");
+
+	for(var i = 0; i < data.mustListen.length; i++){
+		mustListenList.append($("<li>").append($("<span>").html(data.mustListen[i])));
+	}
+
+	let songsHeader = $("<p>").addClass("list-header").html("Songs in this album");
+	let songsList = $("<ol>");
+
+	for(var i = 0; i < data.songsInAlbum.length; i++){
+		songsList.append($("<li>").append($("<span>").html(data.songsInAlbum[i])));
+	}
+
+	return $("<main>").append(headerElement).append(artworkElement).append(detailsListHeader).append(detailsList).append(mustListenHeader).append(mustListenList).append(songsHeader).append(songsList);
+}
 
 
 function onMobileNavClick(){
@@ -26,8 +131,6 @@ function onMobileNavClick(){
 	  } else {
 	    mobileLinksElement.style.display = "block";
 	  }
-
-
 }
 
 
@@ -43,13 +146,13 @@ contactForm.addEventListener('submit', (event) => {
 	let formData = new FormData(contactForm)
 	let formObj = {}
 	
-		for (let [key, value] of formData.entries()) { 
-		  formObj[key] = value;
-		}
+	for (let [key, value] of formData.entries()) { 
+	  formObj[key] = value;
+	}
 
-		localStorage.setItem('contactFormData', JSON.stringify(formObj));
+	localStorage.setItem('contactFormData', JSON.stringify(formObj));
 
-		const {name, country} = formObj;
+	const {name, country} = formObj;
 
 	document.cookie=`name=${name}`;
 	document.cookie=`country=${country}`;
@@ -64,10 +167,10 @@ function fixNavBar (){
 
 	if (prevScrollPos > currentScrollPos) {
 		navRef.style.top = "0";
-  } else {
-    navRef.style.top = "-100px";
-  }
- prevScrollPos = currentScrollPos;
+   } else {
+    	navRef.style.top = "-100px";
+  	}
+ 	prevScrollPos = currentScrollPos;
 }
 
 window.addEventListener('scroll', fixNavBar)
---
+++ React_App/waves-of-paramnesia/README.md
@@ -0,0 +1,20 @@
+This is the default structure
+
+### NPM or YARN Commands
+
+```
+yarn start - To start the application
+
+yarn build - To build the application
+
+yarn test - To test the application
+
+yarn lint - To check the lint errors in the application
+
+yarn lint:fix - To check the lint errors and fix the code from the application
+
+yarn format - To format the code in nicer way using prettier
+
+yarn isReady - This is the combination of format and lint:fix
+
+```
